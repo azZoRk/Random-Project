5d8a32b78359ca494f724e815335978c0576d10c
azZoRk <ethanmaxence.m@gmail.com>
2023-05-18 11:07:05 +0200

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+
+low = 'abcdefghijklmnopqrstuvwxyz'
+up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+numbers = "0123456789"
+
+symbols = "[]{}#()@;:/*.-_"
+
+concatenate = low+up+numbers+symbols
+
+lenght_password = 12
+
+password = "" .join(random.sample(concatenate, \
+                        lenght_password ))
+
+print (password)
